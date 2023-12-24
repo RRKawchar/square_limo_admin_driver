a06a45aae7c49f8b24cadf1ad8fca39f0e354b42
RRKawchar <riyazurrohmankawchar@gmail.com>
2023-12-24 18:27:18 +0600

    add dashboard folder

---
+++ lib/admin/src/features/auth/view/screens/a_login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ALoginScreen extends StatefulWidget {
+  const ALoginScreen({super.key});
+
+  @override
+  State<ALoginScreen> createState() => _ALoginScreenState();
+}
+
+class _ALoginScreenState extends State<ALoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/admin/src/features/auth/view/screens/a_sign_up_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ASignupScreen extends StatefulWidget {
+  const ASignupScreen({super.key});
+
+  @override
+  State<ASignupScreen> createState() => _ASignupScreenState();
+}
+
+class _ASignupScreenState extends State<ASignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
